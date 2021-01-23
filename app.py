1364ba97a6ca137dbb7a4da93530b281c249d066
edonahue <erich.donahue@gmail.com>
2021-01-22 22:53:57 -0500

    Formatting initial HTML.

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ app = dash.Dash(__name__)
 server = app.server
 app.layout = html.Div(children=[
     html.H1(children='Positive Grid Spark Amplifier Sales Metrics'),
-    html.Div(children=html.P(children='''
+    html.Div([html.P('''
         Dashboard of sales volume and revenue metrics for the <a href="https://www.positivegrid.com/spark/">Positive Grid Spark amplifier</a> based on <a href="https://api.positivegrid.com/api/counter/spark_preorder_2019">public API response data</a>.
-    ''')),
+    ''')]),
     html.P("Select y-axis"),
     dcc.Dropdown(
         id='y-axis',
